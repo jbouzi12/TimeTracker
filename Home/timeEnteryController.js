855a141653fce88e4d682b379a8fcea2704c4375
Jensen Bouzi <jensen.bouzi@gmail.com>
2015-10-23 15:10:24 -0400

    create task tracker controller for employees

---
+++ Home/timeEnteryController.js
@@ -0,0 +1,72 @@
+(function () {
+
+    angular.module('taskTracker')
+    .controller('timeEntryController', ['$scope','systemLockFactory', '$stateParams', 'databaseService', function ($scope, systemLockFactory, $stateParams, databaseService) {
+
+        // Employee should be found based on StateParams' name attribute
+        //$scope.systemIsLocked = systemLockFactory.isLocked();
+        //console.log($scope.systemIsLocked);
+        $scope.orderBy = '';
+        $scope.status = {
+            opened: false
+        };
+        $scope.today = function () {
+            $scope.dt = new Date();
+        };
+        $scope.today();
+
+        $scope.clear = function () {
+            $scope.dt = null;
+        };
+
+        // Disable weekend selection
+        //$scope.disabled = function (date, mode) {
+        //    return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
+        //};
+
+        $scope.toggleMin = function () {
+            $scope.minDate = $scope.minDate ? null : new Date();
+        };
+        $scope.toggleMin();
+
+        $scope.open = function ($event) {
+            $scope.status.opened = true;
+        };
+        $scope.workTypes = ['Training', 'Analysis','Development', 'Testing', 'Network', 'Proposal', 'Design', 'Travel', 'Recruiting'];
+        $scope.entry = {};
+        $scope.isCollapsed = true;
+
+        var employees = databaseService.getData('employees');
+        $scope.employee = _.find(employees, { name: $stateParams.name });
+
+        $scope.projects = function() {
+            var projects = [];
+            $scope.employee.assignments.forEach(function(assignment) {
+                if (assignment.project != null) {
+                    projects.push(assignment.project);
+                };
+            });
+            return projects;
+        };
+
+        $scope.phases = function() {
+            var phases = [];
+            $scope.employee.assignments.forEach(function(assignment) {
+                if (assignment.phase != null) {
+                    phases.push(assignment.phase);
+                };
+            });
+            return phases;
+        };
+
+        $scope.updateTime = function(entry) {
+            databaseService.updateTime($scope.employee, entry);
+        };
+
+        $scope.addTime = function (entry) {
+            var newEntry = entry;
+            $scope.entry = {};
+            databaseService.addTime($scope.employee, newEntry);           
+        };
+    }]);
+})();
