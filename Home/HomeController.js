9444a9895f8fa0c3fba60849912f2cd82cbda33f
Jensen Bouzi <jensen.bouzi@gmail.com>
2015-10-28 11:54:51 -0400

    add validation to employee signin

--- Home/HomeController.js
+++ Home/HomeController.js
@@ -1,9 +1,12 @@
 (function () {
 
     angular.module('taskTracker')
-    .controller('homeController', ['$scope', '$stateParams', function($scope, $stateParams) {
+    .controller('homeController', ['$scope', '$stateParams', 'databaseService', function($scope, $stateParams, databaseService) {
         $scope.name = '';
         $scope.animate = false;
+        $scope.validName = false;
+        $scope.employees = databaseService.getData('employees');
+        $scope.validateName = validateName;
 
         $scope.toggleOpen = function() {
             $scope.animate = !$scope.animate;
@@ -11,5 +14,14 @@
         $scope.cancel = function () {
             $modalInstance.dismiss('cancel');
         };
+
+        function validateName(name, password){
+            console.log('name', name);
+            var employee = _.where($scope.employees, {name: name});
+            console.log('employee', employee);
+            if (employee.length === 1 && password.length) {
+                $scope.validName = true;
+            }
+        }
     }]);
 })();
