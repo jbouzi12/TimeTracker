5985aebfb3ba64d36eabb0f7802b0e0ef9605d01
Jensen Bouzi <jensen.bouzi@gmail.com>
2015-11-30 13:44:21 -0500

    add user auth for sign in in home controller

--- Home/HomeController.js
+++ Home/HomeController.js
@@ -1,9 +1,12 @@
 (function () {
 
     angular.module('taskTracker')
-    .controller('homeController', ['$scope', '$stateParams', function($scope, $stateParams) {
+    .controller('homeController', ['$scope', '$stateParams', 'databaseService', function($scope, $stateParams, databaseService) {
         $scope.name = '';
         $scope.animate = false;
+        $scope.isDisabled = true;
+        $scope.employees = databaseService.getData('employees');
+        $scope.validateName = validateName;
 
         $scope.toggleOpen = function() {
             $scope.animate = !$scope.animate;
@@ -11,5 +14,13 @@
         $scope.cancel = function () {
             $modalInstance.dismiss('cancel');
         };
+
+        function validateName(name, password){
+            var employee = _.where($scope.employees, {name: name});
+            if (employee.length === 1 && password.length >=5) {
+                $scope.isDisabled = false;
+            }
+        };
+
     }]);
 })();
